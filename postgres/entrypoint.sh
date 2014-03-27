81c200850e16f9e28487dd195caf276b2a834639
cfeliciano <c.feliciano2009@gmail.com>
2014-03-26 22:37:53 -0400

    Fixed issues with postgres user permissions

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 if [ ! -d "/var/lib/pgsql/data" ]; then
-	exit 1;
+        exit 1;
 fi
 
 if [ ! -d "/var/lib/pgsql/data/base" ]; then
-	chown postgres:postgres /var/lib/pgsql/data
-	/usr/lib/postgresql/9.3/bin/initdb -D /var/lib/pgsql/data
-	/usr/lib/postgresql/9.3/bin/postgres --config-file=/etc/postgresql/9.3/main/postgresql.conf > /dev/null &
-	# hack
-	sleep 3
+        chown postgres:postgres /var/lib/pgsql/data
+        su postgres -c "/usr/lib/postgresql/9.3/bin/initdb -D /var/lib/pgsql/data"
+        su postgres -c "/usr/lib/postgresql/9.3/bin/postgres --config-file=/etc/postgresql/9.3/main/postgresql.conf > /dev/null &"
+        # hack
+        sleep 3
 
-	# DATABASE NAMES
-	while read p; do
-		/usr/lib/postgresql/9.3/bin/createdb $p
-	done < databases.txt
+        # DATABASE NAMES
+        while read p; do
+                su postgres -c "/usr/lib/postgresql/9.3/bin/createdb $p"
+        done < databases.txt
 
-	psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+        su postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'postgres';\""
 
-	kill `ps aux | grep '/bin/postgres' | awk '{print $2}'`
-	# hack
-	sleep 3
+        kill `ps aux | grep '/bin/postgres' | awk '{print $2}'`
+        # hack
+        sleep 3
 fi
 
-exec /usr/lib/postgresql/9.3/bin/postgres --config-file=/etc/postgresql/9.3/main/postgresql.conf
+exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres --config-file=/etc/postgresql/9.3/main/postgresql.conf"
